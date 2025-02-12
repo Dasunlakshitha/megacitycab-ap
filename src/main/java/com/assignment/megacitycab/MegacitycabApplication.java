1abde6312f3389cf73aec5258dfd3f8d26df0eda
dasuncey <158262132+dasuncey@users.noreply.github.com>
2025-02-12 08:03:53 +0530

    init

---
+++ src/main/java/com/assignment/megacitycab/MegacitycabApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.megacitycab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegacitycabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegacitycabApplication.class, args);
+	}
+
+}
